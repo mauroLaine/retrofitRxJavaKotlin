4238aaa3396724c2f6ad749aa6f6445c1f521cb5
Mauricio Laine <laine.mau@gmail.com>
2019-08-20 21:13:53 -0700

    created the service call generator and the user service contract

--- app/src/main/java/com/laine/mauro/retrofitrxjavakotlin/data/GitHubServiceGenerator.kt
+++ app/src/main/java/com/laine/mauro/retrofitrxjavakotlin/data/GitHubServiceGenerator.kt
@@ -1,6 +1,34 @@
 package com.laine.mauro.retrofitrxjavakotlin.data
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class GitHubServiceGenerator {
 
+    companion object {
+        private val BASE_URL = "https://api.github.com/"
 
+        private val okHttpClient: OkHttpClient.Builder = OkHttpClient.Builder()
+
+        private val httpLoginInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
+
+        private val retrofitBuilder: Retrofit.Builder = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient.build())
+
+        private lateinit var retrofit: Retrofit
+
+        fun <S> createService(serviceClass: Class<S>): S {
+            if (!okHttpClient.interceptors().contains(httpLoginInterceptor)) {
+                httpLoginInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+                okHttpClient.addInterceptor(httpLoginInterceptor)
+                retrofitBuilder.client(okHttpClient.build())
+                retrofit = retrofitBuilder.build()
+            }
+            return retrofit.create(serviceClass)
+        }
+    }
 }
--- app/src/main/java/com/laine/mauro/retrofitrxjavakotlin/data/UserService.kt
+++ app/src/main/java/com/laine/mauro/retrofitrxjavakotlin/data/UserService.kt
@@ -1,6 +1,12 @@
 package com.laine.mauro.retrofitrxjavakotlin.data
 
+import com.laine.mauro.retrofitrxjavakotlin.model.Users
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface UserService {
 
+    @GET("/users")
+    fun getUsers(): Call<List<Users.User>>
 
 }
